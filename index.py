426e9460b7805ecf7be06d421c11d7f6df41b9bf
Rayrayeat <113868712+Rayrayeat@users.noreply.github.com>
2022-11-15 11:43:40 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -7,23 +7,21 @@ import json
 import pandas as pd
 
 # Use a service account.
-app = None
-db = None
-if db is None :
-    print("建立firestore實體")
+if 'firestore' not in st.session_state:
+    st.session_state['firestore'] = True   
     key_dict = json.loads(st.secrets["textkey"])
-    cred = credentials.Certificate(key_dict)
-    if app is None:
-        app = firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(key_dict)   
+    app = firebase_admin.initialize_app(cred)
     db = firestore.client()
+    st.session_state['db'] = db
 
+db = st.session_state['db']
 records_ref = db.collection('records')
 query = records_ref.limit_to_last(20)
 docs = query.get()
 
 datalist = [doc.to_dict() for doc in docs]
 dict_data = pd.DataFrame(datalist)
-
 st.title("光線和距離及時監控")
 st.table(dict_data)
 
