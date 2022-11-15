e282768b775d5d959c10ab668992f2faaf9aba1b
Rayrayeat <113868712+Rayrayeat@users.noreply.github.com>
2022-11-15 10:40:09 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,5 +1,21 @@
 import streamlit as st
+
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+
+# Use a service account.
+app = None
+db = None
+if db is None :
+    print("建立firestore實體")
+    key_dict = json.loads(st.secrets["textkey"])
+    cred = credentials.Certificate(key_dict)
+    if app is None:
+        app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
 
 
 st.title("光線和距離及時監控")
